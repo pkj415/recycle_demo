a694f2b10785222fbeaf291743d4189e490ebaf6
Piyush Jain <piyushjain1996@gmail.com>
2020-03-29 10:52:11 -0500

    More changes

--- app.py
+++ app.py
@@ -9,6 +9,30 @@ api = Api(app, version="1.0", title="rePurpose Plastic token", validate=False)
 
 plastic_coin = api.namespace('plastic_coin', description='Plastic coin entity')
 
+mint_request = api.model('mint_request', {
+    'client_public_key': fields.String(required=True, default="0x1F0a4a146776ECC2a3e52F6700901b51aE528bBC", description='Minter address'),
+    'client_nonce': fields.Integer(required=True, default=1, description='Client Nonce'),
+    'offset_amount': fields.Float(required=True, default=10.5, description='Weight of plastic offset'),
+    'pledged_users': fields.Nested(api.model('pledged_users', {
+        'public_key': fields.String(required=True, default="0x1F0a4a146776ECC2a3e52F6700901b51aE528bBC", description='User address'),
+        'share': fields.Integer(required=True, default=1, description='Share of user')
+    })),
+    "request_type": fields.String(required=True, default="create_coin", description='Request type'),
+    'stages': fields.Nested(api.model('stages', [
+        {
+            'name': fields.String(required=True, default="Transport", description='Name of stage'),
+            'documents': fields.Nested(api.model('documents', [
+                {
+                    'location': fields.String(required=True, default="aws/s3", description='Location of document'),
+                    'hash': fields.String(required=True, default="0x1F0a4a146776ECC2a3e52F6700901b51aE528bBC", description='SHA hash of document')
+                }
+            ])),
+        }
+    ])),
+    'version': fields.Integer(required=True, default=1, description='Version of the coin'),
+    'transaction_signature': fields.String(required=True, default="", description='Signature of payload')
+})
+
 @plastic_coin.route('')
 class CreatePlasticCoin(Resource):
     @api.expect(mint_request)
@@ -23,6 +47,7 @@ class CreatePlasticCoin(Resource):
             status=200, mimetype='application/json')
 
         return resp
+
 
 def main():
     import sys
